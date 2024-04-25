28557ee085335e46afa1e7517f32d2562ec16d05
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-04-25 03:03:59 -0500

    it quits, for some reason.

--- todolist-client.lua
+++ todolist-client.lua
@@ -2,44 +2,86 @@ local event = require "event"
 local term = require "term"
 local fs = require "filesystem"
 local component = require "component"
+local serialization = require "serialization"
+
+function loadServerList()
+    if fs.exists("/home/serverlist.txt") then
+        local file = io.open("/home/serverlist.txt", "r")
+        local serverList = serialization.unserialize(file:read("*all"))
+        file:close()
+        return serverList
+    else
+        return {}
+    end
+end
+
+function saveServerList(serverList)
+    local file = io.open("/home/serverlist.txt", "w")
+    file:write(serialization.serialize(serverList))
+    file:close()
+end
 
 function start()
-    -- change serverAddress to the address of network card
-    -- of the computer running the server
-    local serverAddress = "c4de7dc5-c340-4bb8-9ccf-1939f9eaf27a"
-    local port = 4000
     term.clear()
-    print("attempting request of updated todolist from todo-server at "..string.sub(serverAddress, 1,8))
+    local serverList = loadServerList()
+
+    print("Select a server address:")
+    for i, server in ipairs(serverList) do
+        print(i .. ". " .. server)
+    end
+    print("Enter a new server address or select a number from the list:")
+
+    local input = io.read()
+    local serverAddress
+
+    if tonumber(input) then
+        local index = tonumber(input)
+        if index >= 1 and index <= #serverList then
+            serverAddress = serverList[index]
+        else
+            print("Invalid selection. Please try again.")
+            return
+        end
+    else
+        serverAddress = input
+        if not serverList[serverAddress] then
+            table.insert(serverList, serverAddress)
+            saveServerList(serverList)
+        end
+    end
+
+    local port = 4000
+    print("Attempting request of updated todolist from todo-server at " .. string.sub(serverAddress, 1, 8))
 
     if not component.isAvailable("modem") then
-        print("failed to request todolist: this computer lacks a network card.")
+        print("Failed to request todolist: this computer lacks a network card.")
         return
     end
     local modem = component.modem
 
     if not modem then
-        print("failed to request todolist: this computer lacks a network card.")
+        print("Failed to request todolist: this computer lacks a network card.")
         return
     end
 
     if not modem.open(port) then
-        print("failed to open port " .. port)
+        print("Failed to open port " .. port)
         return
     end
 
     local path = "/home/todolist.txt"
     local f, err = io.open(path, "w")
     if not f then
-        print(string.format("failed to open %s for writing: %s", path, err))
+        print(string.format("Failed to open %s for writing: %s", path, err))
         modem.close(port)
         return
     end
 
     modem.send(serverAddress, port, "request-todo")
     local timeout = 5
-    local _,_,fromAddress,_,_,msg = event.pull(timeout, "modem_message")
+    local _, _, fromAddress, _, _, msg = event.pull(timeout, "modem_message")
     if not msg then
-        print("response timed out after " .. timeout .. " seconds")
+        print("Response timed out after " .. timeout .. " seconds")
         modem.close(port)
         return
     end
@@ -59,7 +101,7 @@ function start()
     print(horzLine)
     print("\n")
 
-    print("press any key to continue...")
+    print("Press any key to continue...")
     event.pull("key_down")
     term.clear()
 end
